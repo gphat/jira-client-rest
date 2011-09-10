e7474f4b4d8e973dd7b4e67703cf1f32e769c736
Cory G Watson <gphat@Corys-iMac.local>
2011-09-10 16:31:31 -0500

    Add new methods.

--- lib/JIRA/Client/REST.pm
+++ lib/JIRA/Client/REST.pm
@@ -3,7 +3,28 @@ use Moose;
 
 # ABSTRACT: JIRA REST Client
 
+use JSON qw(decode_json encode_json);
 use Net::HTTP::Spore;
+
+=head1 DESCRIPTION
+
+JIRA::Client::REST is a wrapper for the L<JIRA REST API|http://docs.atlassian.com/jira/REST/latest/>.
+It is a thin wrapper, returning decoded version of the JSON without any munging
+or mangling.
+
+=head1 SYNOPSIS
+
+    use JIRA::Client::REST;
+
+    my $client = JIRA::Client::REST->new(
+        username => 'username',
+        password => 'password',
+        url => 'http://jira.mycompany.com',
+    );
+    my $issue = $client->get_issue('PRGPRESENTS-12');
+    print $issue->{fields}->{priority}->{value}->{name}."\n";
+
+=cut
 
 has '_client' => (
     is => 'rw',
@@ -21,7 +42,77 @@ has '_client' => (
                         "required_params": [
                             "id"
                         ],
+                        "optional_params": [
+                            "expand"
+                        ],
                         "method": "GET",
+                        "authentication": true
+                    },
+                    "get_issue_transitions": {
+                        "path": "/rest/api/latest/issue/:id/transitions",
+                        "required_params": [
+                            "id"
+                        ],
+                        "optional_params": [
+                            "expand"
+                        ],
+                        "method": "GET",
+                        "authentication": true
+                    },
+                    "get_issue_votes": {
+                        "path": "/rest/api/latest/issue/:id/votes",
+                        "required_params": [
+                            "id"
+                        ],
+                        "optional_params": [
+                            "expand"
+                        ],
+                        "method": "GET",
+                        "authentication": true
+                    },
+                    "get_issue_watchers": {
+                        "path": "/rest/api/latest/issue/:id/watchers",
+                        "required_params": [
+                            "id"
+                        ],
+                        "optional_params": [
+                            "expand"
+                        ],
+                        "method": "GET",
+                        "authentication": true
+                    },
+                    "unvote_for_issue": {
+                        "path": "/rest/api/latest/issue/:id/votes",
+                        "required_params": [
+                            "id"
+                        ],
+                        "method": "DELETE",
+                        "authentication": true
+                    },
+                    "unwatch_issue": {
+                        "path": "/rest/api/latest/issue/:id/watchers",
+                        "required_params": [
+                            "id",
+                            "username"
+                        ],
+                        "method": "DELETE",
+                        "authentication": true
+                    },
+                    "vote_for_issue": {
+                        "path": "/rest/api/latest/issue/:id/votes",
+                        "required_params": [
+                            "id"
+                        ],
+                        "method": "POST",
+                        "authentication": true
+                    },
+                    "watch_issue": {
+                        "path": "/rest/api/latest/issue/:id/watchers",
+                        "required_params": [
+                            "id",
+                            "username"
+                        ],
+                        "method": "POST",
                         "authentication": true
                     }
                 }
@@ -60,7 +151,7 @@ Set/Get the URL for the JIRA instance.
 
 has 'url' => (
     is => 'rw',
-    isa => 'URI',
+    isa => 'Str',
     required => 1
 );
 
@@ -76,11 +167,100 @@ has 'username' => (
     required => 1
 );
 
-sub get_issue {
-    my $self = shift;
+=method get_issue($id, $expand)
 
-    use Data::Dumper;
-    print Dumper($self->_client->get_issue(@_));
+Get the issue with the supplied id.  Returns a HashRef of data.
+
+=cut
+
+sub get_issue {
+    my ($self, $id, $expand) = @_;
+
+    return $self->_client->get_issue(id => $id, expand => $expand);
+}
+
+=method get_issue_transitions($id, $expand)
+
+Get the transitions possible for this issue by the current user.
+
+=cut
+
+sub get_issue_transitions {
+    my ($self, $id, $expand) = @_;
+
+    return $self->_client->get_issue_transitions(id => $id, expand => $expand);
+}
+
+=method get_issue_votes($id, $expand)
+
+Get voters on the issue.
+
+=cut
+
+sub get_issue_votes {
+    my ($self, $id, $expand) = @_;
+
+    return $self->_client->get_issue_votes(id => $id, expand => $expand);
+}
+
+=method get_issue_watchers($id, $expand)
+
+Get watchers on the issue.
+
+=cut
+
+sub get_issue_watchers {
+    my ($self, $id, $expand) = @_;
+
+    return $self->_client->get_issue_watchers(id => $id, expand => $expand);
+}
+
+=method unvote_for_issue($id)
+
+Remove your vote from an issue.
+
+=cut
+
+sub unvote_for_issue {
+    my ($self, $id) = @_;
+
+    return $self->_client->unvote_for_issue(id => $id);
+}
+
+=method unwatch_issue($id, $username)
+
+Remove a watcher from an issue.
+
+=cut
+
+sub unwatch_issue {
+    my ($self, $id, $username) = @_;
+
+    return $self->_client->unwatch_issue(id => $id, username => $username);
+}
+
+=method vote_for_issue($id)
+
+Cast your vote in favor of an issue.
+
+=cut
+
+sub vote_for_issue {
+    my ($self, $id) = @_;
+
+    return $self->_client->vote_for_issue(id => $id);
+}
+
+=method watch_issue($id, $username)
+
+Watch an issue. (Or have someone else watch it.)
+
+=cut
+
+sub watch_issue {
+    my ($self, $id, $username) = @_;
+
+    return $self->_client->watch_issue(id => $id, username => $username);
 }
 
 1;
